c8cc02cba5a515da2365888d622fa8438eb540b0
GersonEmanuel <gersonemanuel432@gmail.com>
2025-05-19 22:49:04 -0300

    .

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$*+p5evt%g@u9qjghr_col&pz1pw*@t84gw+a=r9mg47qr^5dg'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
