e72b9d272454864ea7807fdff7ad32230e20deed
GersonEmanuel <gersonemanuel432@gmail.com>
2025-05-23 23:54:11 -0300

    chatroomview

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
+from .serializers import ChatRoomSerializer, ChatmessageSerializer
+from .models import ChatRoom, ChatMessage
 # Create your views here.
+
+class ChatRoomView(APIView):
+    def get(self, request, user_id):
+        chat_rooms = ChatRoom.objects.filter(member=user_id)
+        serializer = ChatRoomSerializer(
+            chat_rooms,
+            many=True,
+            context={"request":request}
+        )
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serialzer = ChatRoomSerializer(
+            data=request.data,
+            context={"request":request}
+        )
+        if serialzer.is_valid():
+            serialzer.save()
+            return Response(serialzer.data, status=status.HTTP_200_OK)
+        return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
