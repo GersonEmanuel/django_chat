4b4233e809cfa97fb8305f64c8bbcd94b6813cb9
GersonEmanuel <gersonemanuel432@gmail.com>
2025-05-24 10:54:09 -0300

    vie

--- user/serializers.py
+++ user/serializers.py
@@ -4,6 +4,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import User
+from chat.models import ChatRoom
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,9 +12,62 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'first_name', 'last_name']
 
+
 class LoginSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
         token['user_id'] = user.id
         return token
+    
+
+class SignupSerializer(TokenObtainPairSerializer):
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        required = True,
+        validators=[validate_password]
+    )
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'first_name', 'last_name', 'email', 'password', 'password2'
+        ]
+        extra_kwargs = {
+            'first_name': {'required':True},
+            'last_name': {'required': True},
+            'email': {'required': True},
+            'password': {'required':True}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password': "passwords fields didnt match"}
+            )
+        return attrs
+    
+    
+    
+    @transaction.atomic
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        chat_room = ChatRoom.objects.create(
+            type='SELF', name=user.first_name
+        )
+        chat_room.member.add(user.id)
+        return user
--- user/views.py
+++ user/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import (
+    UserSerializer, LoginSerializer, SignupSerializer
+)
 
 # Create your views here.
+
+class UserView(CreateAPIView):
+    queryset = User.objects.all().order_by('first_name')
+    serializer_class = UserSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_queryset(self):
+        exclude_users_id = []
+        try:
+            exclude_users = self.request.query_params.get('exclude')
+            if exclude_users:
+                users_id = exclude_users.split(',')
+                for user in users_id:
+                    exclude_users_id.append(int(user))
+        except:
+            return []
+        return super().get_queryset().exlude(id__in=exclude_users_id)
+    
+
+
+
+
+class LoginApiVIew(TokenObtainPairView):
+    permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
+
+
+
+class SignupApiView(CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
